5f39530e454bad49057a97da130fb9a91009d6ce
C-P-R-DON <143618073+C-P-R-DON@users.noreply.github.com>
2026-08-12 21:27:32 -0500

    Update model.py

--- birdsong_model/model.py
+++ birdsong_model/model.py
@@ -66,7 +66,7 @@ def pad_collate_fn(batch):
     for item in batch:
         x = item[0] # Extract the tensor from the tuple
         seq_len = x.shape[1]
-        lengths.append(seq_length)
+        lengths.append(seq_len)
         
         pad_amount = max_len - seq_len
         padded_x = F.pad(x, (0, pad_amount), mode='constant', value=0.0)
